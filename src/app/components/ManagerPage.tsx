e6ff64e0ed337d8ac7f0b9c2434018215c80a30a
HoangKhuyen19 <hoangkhuyen1932003@gmail.com>
2023-12-12 11:00:45 +0700

    test2

--- src/app/components/ManagerPage.tsx
+++ src/app/components/ManagerPage.tsx
@@ -44,7 +44,7 @@ export default function ManagerPage() {
             <div className="div-content-left">
                 {/* Header */}
                 <div className="div-header" >
-                    <label className="viva" onClick={() => handlerTabClick(null)}>Viva Coffee </label>
+                    <label className="viva" onClick={() => handlerTabClick(null)}>Viva Coffee Manager </label>
 
                 </div>
                 <div className="div-user">
